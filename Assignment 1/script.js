b83a5bbac04160c10dafe993baef15e38103faed
Ian Bajwa <ian.bajwa@gmail.com>
2022-10-04 02:02:16 -0700

    finished assignment 1

--- Assignment 1/script.js
+++ Assignment 1/script.js
@@ -1,71 +1,46 @@
-//inner html to add html elements
 
-const table = document.querySelector("#dataEntry");
-table.innerHTML = `<tr><th>Name</th><th>Email</th><th>Student ID</th></tr>
-<tr>
-    <td>Ian</td>
-    <td>ian.bajwa@gmail.com</td>
 
-    </tr>
+let form = document.getElementById("formSubmit");
 
-    `;
+let table = document.getElementById('tableData');
 
-    // Adding text using Text Content
 
-    const paragraph = document.querySelector("p");
-    paragraph.textContent = "Hello this is a paragraph";
+//submit button function
 
-    // ***********************
+const submit=()=>{
 
-    //Creating elements using Javascript
+    //variables that use the entered id values
+  let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let id = document.getElementById("id").value;
 
-    const displayTable = document.createElement("table");
-// const th = document.createElement("th");
+    //Alert popups if user misses entry, otherwise it will continue to display data
+if (name == "")
+alert("Enter Name please");
+else if (email == "")
+alert("Enter Email please");
+else if (id == "")
+alert("Enter ID please");
 
-let tableRowLength = 5;
-let tableDataLength = 4;
-for (let i =0; i < tableRowLength; i++) {
-    // console.log("called")
-    const tr = document.createElement("tr");
-
-console.log(displayTable);
-
-for (let j=0; j < tableDataLength; j++) {
-    const td = document.createElement("td");
-    td.textContent = j;
-    td.classList.add("border");
-    tr.appendChild(td);
+else{
+    // loops through the array, the attributes are added to li tags as text and then displayed as table data
     
+  let newArray = [name,email,id];
+  newArray.forEach((item)=>{
+      var li = document.createElement("li");
+  var text = document.createTextNode(item);
+
+  //created li tag gets appended with the item text and then added into the data table, this continues until each entered value is finished
+  li.appendChild(text);
+  table.appendChild(li);
+  
+  })
+//   resets the input boxes after pressing submit button
+  form.reset(); 
+}
 }
 
-displayTable.appendChild(tr);
-}
-
-const displayTableContainer = document.querySelector("#displayTableContainer");
-displayTableContainer.appendChild(displayTable);
-console.log(displayTable);
-
-function greeting() {
-    alert(" Hello Ian");
-}
-
-let userlist = [];
-
-function submitForm() {
-    let data = {
-        name : "",
-        email: "",
-    }
-    
-    //Saving new form submit data
-    userlist.push(data);
-    localStorage.setItem("userlist", JSON.stringify(userlist))
-} 
-
-//You can get the userlist using localstorage, JSON.parse helps maintain it's array form rather than a simple string
-
-let updatedUserList = JSON.parse(localStorage.getItem("userlist"));
-
-for (let i=0; i < updatedUserList.length; i++) {
-
-}
+//Event runs for submit function
+form.addEventListener("submit",(e)=>{
+    submit();
+  })
